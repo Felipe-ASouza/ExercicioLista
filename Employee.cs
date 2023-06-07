51d0edb0d9f92087be88d66a3a09c1873346dd99
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-06 23:58:10 -0300

    Exercício feito com Lista, para cadastrar funcionarios, localizar um funcionario pelo ID, e adicionar uma porcentagem no salário pelo ID

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace ExercicioLista
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(double porcentagem) //aumentar salário
+        {
+            Salary += Salary * porcentagem / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return Id
+                   + ", "
+                   + Name
+                   + ", "
+                   + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioLista
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("How many employees will be registered? "); //Quantos funcionários serão cadastrados?
+            int n = int.Parse(Console.ReadLine());
+            
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("Employee #" + i + ":");
+                Console.Write("Id: "); 
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                list.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+            
+            Console.Write("Enter the employee id that will h ave salary increase: "); //Informe o ID do funcionário que terá aumento salarial
+            int searchID = int.Parse(Console.ReadLine());
+
+            Employee emp = list.Find(x => x.Id == searchID); //Verifica se o ID informado existe na lista de funcionários cadastrados.
+            if (emp != null) //se o ID do funcionário nao for null (Se o funcionário existir)
+            {
+                Console.Write("Enter the percentage: "); //entre com a porcentagem
+                double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                emp.IncreaseSalary(porcentagem);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("This id does not exist!");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Update list of Employees");
+            foreach (Employee obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            
+
+        }
+    }
+}
